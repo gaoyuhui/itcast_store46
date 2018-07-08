5f66609a6a6f24b66bbbd96af9a9967f94640a85
yxp <1206558198@qq.com>
2018-07-08 11:06:04 +0800

    登录-页面

--- src/main.js
+++ src/main.js
@@ -5,16 +5,17 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import '@/assets/css/index.css'
+import MyAxios from '@/plugins/MyAxios'
 
 Vue.config.productionTip = false
-
+Vue.use(MyAxios)
 Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  ElementUI,
   components: { App },
   template: '<App/>'
 })
